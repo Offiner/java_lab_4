f93cb20b2267087ab4e2aec565040cfff973d0c0
Your Name <offiner@gmail.com>
2021-12-27 21:28:47 +0100

    zad 4 zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.awt.dnd.InvalidDnDOperationException;
+import java.lang.reflect.Method;
 import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,27 +22,26 @@ public class Main {
            boolean isIndexCorrect
            white(!isIndexCorrect) {}
         */
-        int [] tab = new int[]{1,2,3,4,5,6};
-        String nrIndeksu="0";
+        String[] tab = new String[]{"zero", "jeden", "dwa", "trzy", "cztery", "pięć"};
+        String nrIndeksu = "0";
+        boolean isString = false;
         Scanner scan = new Scanner(System.in);
-        int liczba=0;
-        do{
-            if(liczba != 0)
+        int liczba = 0;
+        do {
+            if ((liczba < 0 | liczba > 5) & isString != true)
                 System.out.println("Niepoprawny numer!");
             System.out.println("Wprowadź numer indeksu: ");
             nrIndeksu = scan.next();
             try {
                 //liczba = Double.parseDouble(text);
                 liczba = Integer.parseInt(nrIndeksu);
-            }
-            catch(NumberFormatException exception){
+            } catch (NumberFormatException exception) {
+                isString = true;
                 System.out.println("Nie podano liczby");
-                break;
+
             }
-            liczba = Integer.parseInt(nrIndeksu);
-
-
-        }while (liczba > 5 | liczba < 0 );
+        } while ((liczba > 5 | liczba < 0));
+        System.out.println(tab[liczba]);
 
         /* zad.2
         a) stworzyć dowolny przykład z NullPonterException,
@@ -47,5 +49,37 @@ public class Main {
         c) stworzyć dowolny przykład z 2 lub 3 blokami catch i z blokiem finally,
          */
 
-    }
-}
+        //a
+        String tab2[] = new String[4];
+        try {
+            for (int i = 0; i < tab2.length; i++)
+            if (tab2[i].startsWith("Kazik"));
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerExcep...");
+        }
+
+        //b
+        int wynik;
+        try{
+            wynik = 10/0;
+        }catch (ArithmeticException e){
+            System.out.println("Aritmethic exceprtion!!!");
+        }
+        //c
+        String [] tab3 = new String[]{"Pierwszy","Drugi","Trzeci"};
+        try{
+
+            System.out.println(tab3[scan.nextInt()]);
+
+        } catch(ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Index poza tablicą!");
+        } catch(ClassCastException exception) {
+            System.out.println("Class coast!!!");
+        } catch(InvalidDnDOperationException exception) {
+            System.out.println("Zły DND!!");
+        }
+
+    }}
+
+
